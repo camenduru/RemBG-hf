f4d8a87a82581b3319d1bee5ba3ed1f06532c9a3
KenjieDec <kenjiedec@gmail.com>
2022-12-13 23:01:35 +0700

    Sorry :(

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ import gradio as gr
 import os
 import cv2
 
-def inference(file, mask, af):
+def inference(file, af, mask, model):
     im = cv2.imread(file, cv2.IMREAD_COLOR)
     cv2.imwrite(os.path.join("input.png"), im)
 
     from rembg import remove
+    from rembg.session_base import BaseSession
+    from rembg.session_factory import new_session
 
     input_path = 'input.png'
     output_path = 'output.png'
@@ -17,7 +19,15 @@ def inference(file, mask, af):
     with open(input_path, 'rb') as i:
         with open(output_path, 'wb') as o:
             input = i.read()
-            output = remove(input, alpha_matting_erode_size = af, only_mask = (True if mask == "Mask only" else False))
+            sessions: dict[str, BaseSession] = {}
+            output = remove(
+                input, 
+                session=sessions.setdefault(
+                    model, new_session(model)
+                ),
+                alpha_matting_erode_size = af, 
+                only_mask = (True if mask == "Mask only" else False)
+            )
             o.write(output)
     return os.path.join("output.png")
         
@@ -37,14 +47,25 @@ gr.Interface(
                 "Mask only"
             ], 
             type="value",
-            default="Alpha matting",
+            default="Default",
             label="Choices"
-        )
+        ),
+        gr.inputs.Dropdown([
+            "u2net", 
+            "u2netp", 
+            "u2net_human_seg", 
+            "u2net_cloth_seg", 
+            "silueta"
+            ], 
+            type="value",
+            default="u2net",
+            label="Models"
+        ),
     ], 
     gr.outputs.Image(type="file", label="Output"),
     title=title,
     description=description,
     article=article,
-    examples=[["lion.png", 10, "Default"], ["girl.jpg", 10, "Default"]],
+    examples=[["lion.png", 10, "Default", "u2net"], ["girl.jpg", 10, "Default", "u2net"]],
     enable_queue=True
     ).launch()
